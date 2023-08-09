6926de3436bb0ac75a06cd77477b1ede0b942196
Uche Odinamba <uchay2x@icloud.com>
2023-08-09 15:18:15 +0200

    3001

--- api/index.js
+++ api/index.js
@@ -258,6 +258,6 @@ app.delete("/post/:id", async (req, res) => {
 
 module.exports = app;
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
 });

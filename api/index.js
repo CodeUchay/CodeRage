bf7a9072029695812ed37f29033f7f933f54d2ff
Uche Odinamba <uchay2x@icloud.com>
2023-08-09 14:15:06 +0200

    fix api bugs

--- api/index.js
+++ api/index.js
@@ -255,16 +255,5 @@ app.delete("/post/:id", async (req, res) => {
   });
 });
 
-// Handle unhandled routes
-app.use((req, res, next) => {
-  res.status(404).json({ error: "Route not found" });
-});
-
-// Global error handler
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ error: "Internal Server Error" });
-});
-
 
 module.exports = app;
